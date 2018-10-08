89d5abae89b42040c661c4a0f97a580e777e2a3b
monicaaa <himonicaking@gmail.com>
2018-10-08 16:54:20 -0400

    added slider for year and radio buttons for sex

--- app.r
+++ app.r
@@ -11,6 +11,15 @@ ui <- fluidPage(
   fluidPage(
     
     sidebarPanel(
+      # Copy the line below to make a slider range 
+      sliderInput("slider", label = h3("Year Range"), min = 1934, 
+                  max = 2014, value = c(1934, 2014)),
+  
+      # Copy the line below to make a set of radio buttons
+      radioButtons("radio", label = h3("Select Sex"),
+                   choices = list("Male" = "Male", "Female" = "Female"), 
+                   selected = "Male"),
+      
       checkboxGroupInput("age", "Age Groups:",  
                   c("Adjusted" = "adjusted",
                     "Ages 5-14" = "age05.14",
@@ -19,7 +28,10 @@ ui <- fluidPage(
                     "Ages 45-64" = "age45.64",
                     "Ages 65-84" = "age65.84"),
                   selected = c("adjusted"))
+    
     ),
+    
+
     
     mainPanel(
       plotOutput("mv_plot", click = "plot_click"),
@@ -34,13 +46,15 @@ mv_data <- read.csv(url("https://s3.amazonaws.com/shiny-app-data/absolute_differ
 server <- function(input, output) {
   
   output$mv_plot <- renderPlot({mv_data %>% 
-    filter(age.cat %in% input$age) %>%
+    filter(age.cat %in% input$age,
+           gender == input$radio,
+           year %in% (input$slider[1]:input$slider[2])) %>%
       ggplot(aes(x=year, y=bw_asdr.s, group = age.cat, colour = age.cat)) +
 #      scale_colour_manual(values = c("#000000", "#e41a1c", "#377eb8", "#4daf4a",
   #                                   "#984ea3", "#ff7f00")) +
       geom_hline(yintercept=1, color = "grey") +
       geom_line() +
-      facet_grid(gender ~ .) +  theme(legend.position="bottom") +
+      theme(legend.position="bottom") +
       theme_bw()  +
       theme(text = element_text(size=16))+ 
       theme(legend.position="bottom") +
